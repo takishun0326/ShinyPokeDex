780ae15d6855b0273d634267d2fb6abe5dd2d655
takishun0326 <takishun.st3260@gmail.com>
2025-03-31 21:53:46 +0900

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
+  postcss: {
+    plugins: { tailwindcss: {} },
+  },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+})
